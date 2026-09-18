eae47831be4ce1f91e5c1da93d7041f5fe34bf1a
Jonathan Simeone <33371255+jonathansim94@users.noreply.github.com>
2026-09-18 10:28:10 +0200

    updated timeout

--- utils/simplify_stress_test_csv.py
+++ utils/simplify_stress_test_csv.py
@@ -348,7 +348,7 @@ async def main(
         max_keepalive_connections=number_of_requests,
     )
 
-    timeout = httpx.Timeout(600.0)
+    timeout = httpx.Timeout(1200.0)
 
     start_event = asyncio.Event()
     write_lock = asyncio.Lock()
